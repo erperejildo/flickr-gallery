3f4eb427f865e22192b8eb61da76940c5ac1e4ef
Erperejildo <info@danielrodriguez.eu>
2015-08-11 00:27:29 +0200

    Change package name

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('repositoriosApp'));
+  beforeEach(module('flickrGalleryApp'));
 
   var MainCtrl,
     scope;
